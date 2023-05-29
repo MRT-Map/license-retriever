53f87e0982c1238cd16dfcf6011d84e3666ab1e9
7d <61975820+iiiii7d@users.noreply.github.com>
2023-05-30 00:59:27 +0800

    added errors + touchups

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,45 @@
 use std::{borrow::Cow, collections::HashMap, path::PathBuf};
 
-use anyhow::{anyhow, Result};
 use cargo_metadata::{Metadata, MetadataCommand, Package};
-use futures::future;
+use futures::{executor, future};
 use itertools::Itertools;
 use lazy_regex::{lazy_regex, Lazy, Regex};
 use log::{debug, info, warn};
 use serde::{Deserialize, Serialize};
 use surf::{StatusCode, Url};
+use thiserror::Error;
 use tl::ParserOptions;
 
 pub static DOCS_RS_REGEX: Lazy<Regex> = lazy_regex!(r"^https?://docs\.rs/crate/.*?/.*?/source");
+
+#[derive(Error, Debug)]
+#[non_exhaustive]
+pub enum Error {
+    #[error("http error: {0:?}")]
+    Http(surf::Error),
+    #[error("cargo metadata error: {0:?}")]
+    Metadata(#[from] cargo_metadata::Error),
+    #[error("html parse error: {0:?}")]
+    Html(#[from] tl::ParseError),
+    #[error("Environment variable error: {0:?}")]
+    Env(#[from] std::env::VarError),
+    #[error("Encoding error: {0:?}")]
+    Encode(#[from] rmp_serde::encode::Error),
+    #[error("Decoding error: {0:?}")]
+    Decode(#[from] rmp_serde::decode::Error),
+    #[error("I/O error: {0:?}")]
+    Io(#[from] std::io::Error),
+    #[error("No package menu in `{0}`")]
+    NoPackageMenu(String),
+    #[error("unknown error")]
+    Unknown,
+}
+impl From<surf::Error> for Error {
+    fn from(err: surf::Error) -> Self {
+        Error::Http(err)
+    }
+}
+pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[derive(Clone, Default)]
 pub struct Config {
@@ -57,7 +86,7 @@ fn get_metadata(manifest_path: Option<PathBuf>) -> Result<Metadata> {
     Ok(metadata)
 }
 
-async fn get_license_text_from_url(url: Url) -> surf::Result<String> {
+async fn get_license_text_from_url(url: Url) -> Result<String> {
     debug!("Retrieving license from {url}");
     let content = surf::get(&url).await?.body_string().await?;
     if DOCS_RS_REGEX.is_match(url.as_str()) {
@@ -77,11 +106,16 @@ async fn get_license_text_from_url(url: Url) -> surf::Result<String> {
 
 async fn get_license_texts_from_crates_io_package<'a>(
     package: &Package,
-) -> surf::Result<Vec<String>> {
+    use_latest: bool,
+) -> Result<Vec<String>> {
     let list_url = format!(
         "https://docs.rs/crate/{}/{}/source/",
         package.name,
-        package.version.to_string()
+        if use_latest {
+            "latest".into()
+        } else {
+            package.version.to_string()
+        }
     );
     let mut response = surf::get(&list_url).await?;
     if response.status() == StatusCode::NotFound {
@@ -92,7 +126,7 @@ async fn get_license_texts_from_crates_io_package<'a>(
     let dom = tl::parse(&content, ParserOptions::default())?;
     let n = dom
         .query_selector("a")
-        .ok_or_else(|| anyhow!("No package menu"))?;
+        .ok_or_else(|| Error::NoPackageMenu(list_url.to_owned()))?;
     let futures = n
         .filter_map(|a| a.get(dom.parser()))
         .filter_map(|a| a.as_tag())
@@ -116,7 +150,7 @@ async fn get_license_texts_from_crates_io_package<'a>(
 async fn get_license_texts_from_package<'a>(
     package: &Package,
     config: &'a Config,
-) -> surf::Result<Cow<'a, [String]>> {
+) -> Result<Cow<'a, [String]>> {
     if let Some(license) = config.license_text_overrides.get(&package.name) {
         info!(
             "Retrieving license for {} {} from text overrides",
@@ -135,10 +169,18 @@ async fn get_license_texts_from_package<'a>(
     }
 
     info!(
-        "Attempting to license for {} {} from crates.io",
+        "Attempting to retrieve license for {} {} from crates.io",
         package.name, package.version
     );
-    let licenses = get_license_texts_from_crates_io_package(package).await?;
+    let mut licenses = get_license_texts_from_crates_io_package(package, false).await?;
+
+    if licenses.is_empty() {
+        info!(
+            "Attempting to retrieve license for latest version of {} from crates.io",
+            package.name
+        );
+        licenses = get_license_texts_from_crates_io_package(package, true).await?;
+    }
 
     if licenses.is_empty() {
         warn!("No licenses found for {} {}", package.name, package.version)
@@ -148,10 +190,10 @@ async fn get_license_texts_from_package<'a>(
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct LicenseRetriever<'a>(HashMap<String, Cow<'a, [String]>>);
+pub struct LicenseRetriever<'a>(Vec<(Package, Cow<'a, [String]>)>);
 impl<'a> LicenseRetriever<'a> {
-    pub async fn from_config(config: &'a Config) -> surf::Result<LicenseRetriever<'a>> {
-        let meta = get_metadata(Option::<PathBuf>::None).unwrap();
+    pub async fn async_from_config(config: &'a Config) -> Result<LicenseRetriever<'a>> {
+        let meta = get_metadata(Option::<PathBuf>::None)?;
         let futures = meta
             .packages
             .iter()
@@ -161,30 +203,46 @@ impl<'a> LicenseRetriever<'a> {
             .await?
             .into_iter()
             .zip_eq(&meta.packages)
-            .map(|(licenses, package)| (package.name.to_owned(), licenses))
+            .map(|(licenses, package)| (package.to_owned(), licenses))
             .collect();
         Ok(LicenseRetriever(licenses))
     }
-    pub fn save_to_bytes(&self) -> surf::Result<Vec<u8>> {
-        Ok(rmp_serde::to_vec(&self.0)?)
+    pub fn from_config(config: &'a Config) -> Result<LicenseRetriever<'a>> {
+        executor::block_on(Self::async_from_config(config))
     }
-    pub async fn save_in_out_dir(&self, file_name: &str) -> surf::Result<()> {
+    pub fn to_bytes(&self) -> Result<Vec<u8>> {
+        Ok(rmp_serde::to_vec_named(&self.0)?)
+    }
+    pub async fn async_save_in_out_dir(&self, file_name: &str) -> Result<()> {
         async_fs::write(
-            PathBuf::try_from(std::env::var("OUT_DIR")?)?.join(file_name),
-            self.save_to_bytes()?,
+            PathBuf::from(std::env::var("OUT_DIR")?).join(file_name),
+            self.to_bytes()?,
         )
         .await?;
         Ok(())
     }
-    pub fn load_from_bytes(bytes: &[u8]) -> surf::Result<LicenseRetriever<'static>> {
+    pub fn save_in_out_dir(&self, file_name: &str) -> Result<()> {
+        executor::block_on(self.async_save_in_out_dir(file_name))
+    }
+    pub fn from_bytes(bytes: &[u8]) -> Result<LicenseRetriever<'static>> {
         Ok(LicenseRetriever(rmp_serde::from_slice(bytes)?))
     }
+}
+
+#[macro_export]
+macro_rules! license_retriever_data {
+    ($file_name:literal) => {
+        license_retriever::LicenseRetriever::from_bytes(include_bytes!(concat!(
+            env!("OUT_DIR"),
+            "/",
+            $file_name
+        )))
+    };
 }
 
 #[cfg(test)]
 mod tests {
 
-    use futures::executor;
     use log::LevelFilter;
     use test_log::test;
 
@@ -194,10 +252,10 @@ mod tests {
     fn test() {
         log::set_max_level(LevelFilter::Warn);
         let config = Config::default();
-        let lr = executor::block_on(LicenseRetriever::from_config(&config)).unwrap();
+        let lr = LicenseRetriever::from_config(&config).unwrap();
         assert_eq!(
             lr,
-            LicenseRetriever::load_from_bytes(&lr.save_to_bytes().unwrap()).unwrap()
+            LicenseRetriever::from_bytes(&lr.to_bytes().unwrap()).unwrap()
         );
     }
 }
